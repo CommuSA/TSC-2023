cc1a5e25649ae5d7dc85d5f67e092df60165f60d
Jackieyewang <306981258@qq.com>
2023-03-05 02:01:12 +0800

    Initial abe system implemented

--- Docker/test.sh
+++ Docker/test.sh
@@ -1,6 +1,8 @@
 
-chmod a+X create-ipfs-node
-./create-ipfs-node master 4000 5000 8000
-./create-ipfs-node ipfs-node1 4001 5001 8001
-./create-ipfs-node ipfs-node2 4002 5002 8002
-./create-ipfs-node ipfs-node3 4003 5003 8003
+chmod a+X create-peer-node
+chmod a+X create-master-node
+
+./create-master-node master 4000 5000 8000
+./create-peer-node ipfs-node1 4001 5001 8001
+./create-peer-node ipfs-node2 4002 5002 8002
+./create-peer-node ipfs-node3 4003 5003 8003
